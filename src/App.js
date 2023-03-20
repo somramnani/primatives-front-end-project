a7a28a6fa0085a023f0bb70da79157063213cfaa
somramnani <som.ramnani@gmail.com>
2023-03-20 16:59:30 -0400

    add working code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [tokens, setToken] = useState("");
+
+  // function getData() {
+  //   return axios.get("https://api.primitives.xyz/api/interview/tokens");
+  // }
+  // const data = Promise.all([getData()]);
+
+  // useEffect(() => []);
+
+  // Promise.all(getData()).then(function (data) {
+  //   console.log(data[0].data.tokens.list);
+  //   // setToken(data[0].data.tokens.list);
+  // });
+
+  const getData = async () =>
+    axios({
+      method: "get",
+      url: "https://api.primitives.xyz/api/interview/tokens",
+    }).then(function (response) {
+      let list = response.data.tokens.list;
+      console.log(list);
+      setToken(list);
+    });
+
+  useEffect(() => {
+    getData();
+  });
+  console.log(tokens);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {tokens.map((token) => (
+        <>
+          <img src={token.imageURL} />
+          <h1>{token.name}</h1>
+          <p>Address: {token.address}</p>
+        </>
+      ))}
     </div>
   );
 }
